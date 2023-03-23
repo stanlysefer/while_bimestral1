4df13edfc8022a07609c32ad0bdada4474d61995
sistemas <yerminzonsequeda@colegioguanenta.edu.co>
2023-03-23 15:00:53 -0500

    se hizo lo de el video

---
+++ while_bimestral1_1.py
@@ -0,0 +1,10 @@
+
+import math
+
+n = int(input("Digite un número positivo: "))
+
+while n < 0:
+    print("Error, el número debe ser positivo")
+    n = int(input("Digite un número positivo: "))
+
+print(f"\nLa raiz cuadrada es: {(math.sqrt(n)):.2f}")
---
+++ while_bimestral1_4.py
@@ -0,0 +1,15 @@
+#Ejemplo 1
+x=1
+
+while x<3:
+    print(x)
+    x+=1
+
+print("Fin.")
+
+#Ejemplo 2
+
+x=0
+while x<1000:
+    print("WeyTc")
+    x+=1
